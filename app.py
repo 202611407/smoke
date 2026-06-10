c0a863845bbb84cb0dced9683fd7901a6d58be96
202611407 <2026_11407@kshcm.net>
2026-06-10 11:46:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # ==========================
 # 한글 깨짐 방지
 # ==========================
-plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['font.family'] = 'NanumGothic'
 plt.rcParams['axes.unicode_minus'] = False
 
 # ==========================
